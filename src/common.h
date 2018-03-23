187e814ba4354f580003d9105a9ee60b2d4a7d15
Hojoon <omnibusor@kaist.ac.kr>
2018-03-23 18:57:14 +0900

    Added include guards

--- src/common.h
+++ src/common.h
@@ -1,4 +1,9 @@
+#ifndef COMMON_H
+#define COMMON_H
+
 #define IN
 #define OUT
 
 int get_file_size(IN FILE *fp);
+
+#endif
--- src/pgp_crypto.h
+++ src/pgp_crypto.h
@@ -1,3 +1,7 @@
+#ifndef PGP_CRYPTO_H
+#define PGP_CRYPTO_H
 
 int pgp_enc(char *plain, int plain_size, char *recipient, char **ciphered);
 int pgp_dec(char *ciphered, int ciphered_size, char *passphrase, char **plain);
+
+#endif
