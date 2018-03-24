f0d5b7e3e349f8d530da68051e063156ccf922f9
Park Jung Hwan <ahnmo@pagez.kr>
2018-03-24 17:54:59 +0900

    Update http_client.cc
    Add POST and DELETE

--- server/http_client.cc
+++ server/http_client.cc
@@ -1,9 +1,14 @@
 #include "http_client.h"
 
-int http_get(std::string url) {
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+bool http_get(std::string url, std::string& str) {
   CURL *curl;
   CURLcode res;
-  int ret = 1;
+  int ret = false;
 
   curl = curl_easy_init();
   if (curl) {
@@ -17,7 +22,65 @@ int http_get(std::string url) {
     res = curl_easy_perform(curl);
 
     if(res == CURLE_OK) {
-      ret = 0;
+      ret = true;
+    } else {
+      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    }
+    curl_easy_cleanup(curl);
+  }
+
+  return ret;
+}
+
+bool http_post(std::string url, std::string content, std::string& str) {
+  CURL *curl;
+  CURLcode res;
+  int ret = false;
+
+  curl = curl_easy_init();
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_easy_setopt(curl, CURLOPT_FAILONERROR, true);
+
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, content.c_str());
+
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &str);
+    res = curl_easy_perform(curl);
+
+    if(res == CURLE_OK) {
+      ret = true;
+    } else {
+      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    }
+    curl_easy_cleanup(curl);
+  }
+
+  return ret;
+}
+
+bool http_delete(std::string url, std::string& str) {
+  CURL *curl;
+  CURLcode res;
+  int ret = false;
+
+  curl = curl_easy_init();
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_easy_setopt(curl, CURLOPT_FAILONERROR, true);
+
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &str);
+    res = curl_easy_perform(curl);
+
+    if(res == CURLE_OK) {
+      ret = true;
     } else {
       fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
     }
--- server/http_client.h
+++ server/http_client.h
@@ -4,3 +4,8 @@
 
 #define CURL_STATICLIB
 #include <curl/curl.h>
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp);
+bool http_get(std::string url, std::string& str);
+bool http_post(std::string url, std::string content, std::string& str);
+bool http_delete(std::string url, std::string& str);
