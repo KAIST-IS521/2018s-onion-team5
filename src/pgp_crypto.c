e4fa26fc418f31a6846bfda2fc182bf6517b01ae
Hojoon <omnibusor@kaist.ac.kr>
2018-03-25 16:38:55 +0900

    Modified pgp_enc()

--- src/pgp_crypto.c
+++ src/pgp_crypto.c
@@ -4,35 +4,47 @@
 #include "pgp_crypto.h"
 
 //recipient must be string.
-//plain cannot be '\0'
-int pgp_enc(char *plain, int plain_size, char *recipient, char **ciphered) {
+int pgp_enc(char *input_file_name, char *recipient, char *output_file_name) {
   int ret = -1;
   FILE *file = NULL;
   int cnt = 0;
   int recipient_len = 0;
+	int input_file_name_len = 0;
+	int output_file_name_len = 0;
 
-  /*
-  if (plain[0] == '\0')
-    goto EXIT;
-  */
   recipient_len = strlen(recipient);
+	input_file_name_len = strlen(input_file_name);
+	output_file_name_len = strlen(output_file_name);
 
-  char *command = malloc(sizeof(char) * (plain_size + recipient_len + 200));
+  char *buff = malloc(sizeof(char) * strlen("OK"));
+  if (!buff) {
+    perror("malloc");
+    goto EXIT;
+  }
+  memset(buff, 0, sizeof(char) * strlen("OK"));
+
+  char *command = malloc(sizeof(char) * (input_file_name_len + recipient_len + ouput_file_name_len + 200));
   if (!command) {
     perror("malloc");
     goto EXIT;
   }
-  memset(command, 0, sizeof(char) * (plain_size + recipient_len + 200));
+  memset(command, 0, sizeof(char) * (input_file_name_len + recipient_len + output_file_name_len + 200));
   //printf("malloc size:%zu\n",sizeof(char) * (plain_size + recipient_len + 100));
 
-  strncat(command, "echo -n \"", strlen("echo -n \"") + 1);
-  strncat(command, plain, plain_size + 1);
+  strncat(command, "gpg ", strlen("gpg ") + 1);
+  strncat(command, "-o \"", strlen("-o \"") + 1);
+  strncat(command, output_file_name, output_file_name_len + 1);
   strncat(command, "\" ", strlen("\" ") + 1);
-  strncat(command, "| gpg --encrypt --armor -r \"", strlen("| gpg --encrypt --armor -r \"") + 1);
+  strncat(command, "--encrypt --armor -r \"", strlen("--encrypt --armor -r \"") + 1);
   strncat(command, recipient, recipient_len + 1);
   strncat(command, "\" ", strlen("\" ") + 1);
+  strncat(command, "\"", strlen("\"") + 1);
+  strncat(command, input_file_name, input_file_name_len + 1);
+  strncat(command, "\" ", strlen("\" ") + 1);
   strncat(command, "2>/dev/null ", strlen("2>/dev/null ") + 1);
+  strncat(command, "&& echo -n \"OK\" || echo -n \"KO\"", strlen("&& echo -n \"OK\" || echo -n \"KO\"") + 1);
   //strncat(command, " >> encrypted.asc", 100); // TEMp
+	printf("%s\n",command);
 
   file = popen(command, "r");
   if (!file) {
@@ -40,20 +52,35 @@ int pgp_enc(char *plain, int plain_size, char *recipient, char **ciphered) {
     goto EXIT;
   }
 
-  cnt = fread(*ciphered, sizeof(char), 1024*1024*1024, file);
-  //printf("read_length:%d\n",cnt);
+  cnt = fread(buff, sizeof(char), strlen("OK"), file);
+  printf("read_length:%d\n",cnt);
   //printf("%s\n",*ciphered);
 
   //Check it is encrypted well
+	/*
   if (strstr(*ciphered, "-----BEGIN PGP MESSAGE-----") && strstr(*ciphered, "-----END PGP MESSAGE-----")) {
     ret = 0;
   }
+	*/
+
+  if (cnt == 2) {
+    if (strncmp(buff, "OK", strlen("OK")) == 0) {
+      ret = 0;
+    }
+    else if (strncmp(buff, "KO", strlen("KO")) == 0) {
+      ret = -1;
+    }
+  }
+  //printf("read_length:%d\n",cnt);
+  //printf("%s\n",*plain);
 
 EXIT:
   if (file)
     pclose(file);
   if (command)
     free(command);
+  if (buff)
+    free(buff);
 
   return ret;
 }
@@ -84,8 +111,6 @@ int pgp_dec(char *ciphered, int ciphered_size, char *passphrase, char **plain) {
   strncat(command, "\" ", strlen("\" ") + 1);
   strncat(command, "2>/dev/null ", strlen("2>/dev/null ") + 1);
   strncat(command, "&& echo -n \"OK\" || echo -n \"KO\"", strlen("&& echo -n \"OK\" || echo -n \"KO\"") + 1);
-  //strncat(command, "&& printf \"OK\" || printf \"KO\"", strlen("&& printf \"OK\" || printf \"KO\"") + 1);
-  //strncat(command, " >> encrypted.asc", 100); // TEMp
 
   file = popen(command, "r");
   if (!file) {
