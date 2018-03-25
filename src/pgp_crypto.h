d10b779a0337842478eb6126927da13def0f61de
Hojoon <omnibusor@kaist.ac.kr>
2018-03-25 16:40:53 +0900

    Modified pgp_enc() prototype

--- src/pgp_crypto.h
+++ src/pgp_crypto.h
@@ -1,7 +1,7 @@
 #ifndef PGP_CRYPTO_H
 #define PGP_CRYPTO_H
 
-int pgp_enc(char *plain, int plain_size, char *recipient, char **ciphered);
+int pgp_enc(char *input_file_name, char *recipient, char *output_file_name);
 int pgp_dec(char *ciphered, int ciphered_size, char *passphrase, char **plain);
 
 #endif
