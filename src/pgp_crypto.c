70f5a99bd5c16d3d50c002759af5b73ba63ef057
Hojoon <omnibusor@kaist.ac.kr>
2018-03-25 17:14:20 +0900

    Deleted comments

--- src/pgp_crypto.c
+++ src/pgp_crypto.c
@@ -23,19 +23,19 @@ int pgp_enc(char *input_file_name, char *recipient, char *output_file_name) {
   }
   memset(buff, 0, sizeof(char) * strlen("OK"));
 
-  char *command = malloc(sizeof(char) * (input_file_name_len + recipient_len + ouput_file_name_len + 200));
+  char *command = malloc(sizeof(char) * (input_file_name_len + recipient_len + output_file_name_len + 200));
   if (!command) {
     perror("malloc");
     goto EXIT;
   }
   memset(command, 0, sizeof(char) * (input_file_name_len + recipient_len + output_file_name_len + 200));
-  //printf("malloc size:%zu\n",sizeof(char) * (plain_size + recipient_len + 100));
+  //printf("malloc size:%zu\n",sizeof(char) * (input_file_name_len + recipient_len + output_file_name_len + 200));
 
   strncat(command, "gpg ", strlen("gpg ") + 1);
   strncat(command, "-o \"", strlen("-o \"") + 1);
   strncat(command, output_file_name, output_file_name_len + 1);
   strncat(command, "\" ", strlen("\" ") + 1);
-  strncat(command, "--encrypt --armor -r \"", strlen("--encrypt --armor -r \"") + 1);
+  strncat(command, "--encrypt --armor --yes -r \"", strlen("--encrypt --armor --yes -r \"") + 1);
   strncat(command, recipient, recipient_len + 1);
   strncat(command, "\" ", strlen("\" ") + 1);
   strncat(command, "\"", strlen("\"") + 1);
@@ -44,7 +44,7 @@ int pgp_enc(char *input_file_name, char *recipient, char *output_file_name) {
   strncat(command, "2>/dev/null ", strlen("2>/dev/null ") + 1);
   strncat(command, "&& echo -n \"OK\" || echo -n \"KO\"", strlen("&& echo -n \"OK\" || echo -n \"KO\"") + 1);
   //strncat(command, " >> encrypted.asc", 100); // TEMp
-	printf("%s\n",command);
+	//printf("%s\n",command);
 
   file = popen(command, "r");
   if (!file) {
@@ -53,16 +53,10 @@ int pgp_enc(char *input_file_name, char *recipient, char *output_file_name) {
   }
 
   cnt = fread(buff, sizeof(char), strlen("OK"), file);
-  printf("read_length:%d\n",cnt);
+  //printf("read_length:%d, string: %s\n",cnt,buff);
   //printf("%s\n",*ciphered);
 
   //Check it is encrypted well
-	/*
-  if (strstr(*ciphered, "-----BEGIN PGP MESSAGE-----") && strstr(*ciphered, "-----END PGP MESSAGE-----")) {
-    ret = 0;
-  }
-	*/
-
   if (cnt == 2) {
     if (strncmp(buff, "OK", strlen("OK")) == 0) {
       ret = 0;
