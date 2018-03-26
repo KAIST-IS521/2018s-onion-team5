2186dc12ad6b90ece50aa92cb04c31b3af7eacc4
Park Jung Hwan <ahnmo@pagez.kr>
2018-03-26 10:04:58 +0900

    Make directory which share server and client

---
+++ common/config.cc
@@ -0,0 +1,6 @@
+#include "config.h"
+
+const std::string base_repo = "AhnMo";
+const int BROADCAST_PORT = 9099; // UDP/9099
+const int NODE_PORT = 9099; // TCP/6001
+const int REST_SERV_PORT = 31337; // TCP/31337
---
+++ common/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+extern const std::string base_repo;
+extern const int BROADCAST_PORT;
+extern const int NODE_PORT;
+extern const int REST_SERV_PORT;
